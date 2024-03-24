2868407c382ccdeb098c2a1d50f77a980eb4e396
guylah <guylahavv@gmail.com>
2024-03-24 18:38:19 +0200

    changed app

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def create_conn():
     conn = mysql.connector.connect(
         host="movie-db.cf8oq4agi34u.us-east-1.rds.amazonaws.com",
         user="admin",
-        password="oogg8888",
+        password="",
         database="movie_db"
     )
     return conn
