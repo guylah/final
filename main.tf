8382793d42fe71c52cd2759f9b950269e6755d4c
guylah <guylahavv@gmail.com>
2024-03-25 10:49:25 +0200

    changed vpc

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "192.168.0.0/16"
 
   tags = {
     Name = "my-vpc"
@@ -14,7 +14,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "public_dev_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "192.168.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 
